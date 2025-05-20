0ed814e4ee7b69b5bb9bf3921872562e7e1ddad9
Nelson Albera <nalbera@hotmail.com>
2025-05-20 16:35:06 -0300

    add authentication middleware

--- api-service/src/router/userRouter.routes.js
+++ api-service/src/router/userRouter.routes.js
@@ -2,10 +2,13 @@ import { Router } from "express";
 
 import { loginUserController } from "../controllers/users/index.js";
 
+import authUserMiddleware from "../middlewares/authUserMiddleware.js";
+
 const userRouter = Router();
 
 userRouter.get("/", (req, res) => res.send('User Router'));
 userRouter.post("/login", loginUserController);
 
+userRouter.get('/profile', authUserMiddleware, (req, res) => res.send('paso'));
 
 export default userRouter;
